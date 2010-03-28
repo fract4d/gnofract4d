c20adf086ffdb89a2d5caf1d338d52c04c4c98d7
catenary <catenary@users.sourceforge.net>
2010-03-28 10:04:32 -0700

    cleanup

---
+++ website/mkthumb.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+convert $1.png -resize 150x113 thumbnail_$1.jpg
---
+++ website/upload.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+scp *.html edwin@shell.sourceforge.net:/home/groups/g/gn/gnofract4d/htdocs/
+scp manual/*.html  edwin@shell.sourceforge.net:/home/groups/g/gn/gnofract4d/htdocs/manual
