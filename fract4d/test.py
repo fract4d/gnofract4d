625aca12e780d086b376d9f19b496ac6ff4f8b45
Chris Mayo <aklhfex@gmail.com>
2018-03-04 19:21:11 +0000

    remove unused code in fract4d

--- fract4d/test.py
+++ fract4d/test.py
@@ -28,7 +28,6 @@ import test_browser_model
 import test_formsettings
 import test_ffloat
 import test_absyn
-import test_fractsettings
 
 #import test_stdlib
 
@@ -59,8 +58,7 @@ def suite():
     test_cache.suite(),
     test_browser_model.suite(),
     test_formsettings.suite(),
-    test_ffloat.suite(),
-    test_fractsettings.suite()
+    test_ffloat.suite()
 	#test_stdlib.suite()
     ))
 
