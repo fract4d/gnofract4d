f1222c44a3df139520060932a3e60538e1bdcff9
Guanchor Ojeda <guanchor.ojeda@hyve.net>
2020-03-12 11:19:30 +0000

    [travis] Move pylint options to pylintrc file

--- bin/check-python-code.sh
+++ bin/check-python-code.sh
@@ -1,12 +1,2 @@
 #!/bin/sh
-pylint \
-  -d all \
-  -e bad-whitespace, \
-     trailing-whitespace, \
-     bad-indentation, \
-     wrong-import-position, \
-     unnecessary-semicolon, \
-     trailing-newlines, \
-     bare-except \
-  -s n \
-  fract4d fract4d_compiler fract4dgui
+pylint fract4d fract4d_compiler fract4dgui
