2ab0e915cbc7fad5a524132d919350e8fb5380ae
Guanchor Ojeda <guanchor.ojeda@hyve.net>
2020-03-30 19:35:23 +0100

    [fix] python > fract4dgui gtkfractal > comment not valid method call in exception handler

--- fract4dgui/gtkfractal.py
+++ fract4dgui/gtkfractal.py
@@ -522,8 +522,7 @@ class T(Hidden):
         widget.update_function = set_entry
 
         widget.f = self
-        widget.connect('focus-out-event',
-                       set_fractal, form, order)
+        widget.connect('focus-out-event', set_fractal, form, order)
 
         if hasattr(param, "min") and hasattr(param, "max"):
             # add a slider
@@ -583,7 +582,7 @@ class T(Hidden):
             except Exception as err:
                 msg = "error setting bool param: %s" % str(err)
                 print(msg)
-                utils.idle_add(f.warn, msg)
+                #utils.idle_add(f.warn, msg)
 
             return False
 
@@ -994,8 +993,7 @@ class T(Hidden):
                 y = 0.5 + (self.y + self.newy) / 2.0
 
             # with shift held, don't zoom
-            if hasattr(event, "state") and event.get_state(
-            ) & Gdk.ModifierType.SHIFT_MASK:
+            if hasattr(event, "state") and event.get_state() & Gdk.ModifierType.SHIFT_MASK:
                 zoom = 1.0
             self.recenter(x, y, zoom)
 
