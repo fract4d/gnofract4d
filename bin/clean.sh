d687d7aeb6d5022a9bbed827e966ab1a82922170
Guanchor Ojeda <guanchor.ojeda@hyve.net>
2020-04-20 13:45:52 +0100

    update helper bash scripts

---
+++ bin/clean.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -rf build
+rm fract4d/*.so
+rm -rf .pytest_cache
+rm -rf .tox
--- bin/pylint.sh
+++ bin/pylint.sh
@@ -1,2 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+
+if ! ls fract4d/fract4dc.*.so 1> /dev/null 2>&1;
+then
+  ./bin/build.sh
+fi
+
 pylint *.py fract4d fract4d_compiler fract4dgui
+
+if [[ $? == 0 ]]
+then
+  echo "pylint passed successfully"
+fi
