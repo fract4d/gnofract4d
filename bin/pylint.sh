a6115068754ad34f2c1e72045b55e0ef2a63fef9
Guanchor Ojeda <guanchor.ojeda@hyve.net>
2020-04-20 14:42:53 +0100

    pipeline > update travis and tox deps

--- bin/pylint.sh
+++ bin/pylint.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-if ! ls fract4d/fract4dc.*.so 1> /dev/null 2>&1;
-then
-  ./bin/build.sh
-fi
-
 pylint *.py fract4d fract4d_compiler fract4dgui
 
 if [[ $? == 0 ]]
 then
   echo "pylint passed successfully"
+else
+  exit 1
 fi
