b11b56b10e459a1b4195e27a259d449dc9035c13
Guanchor Ojeda <guanchor@gmail.com>
2020-04-27 13:20:10 +0100

    docker: keep .so files in workspace when using docker scripts

--- docker/bin/run_builds.sh
+++ docker/bin/run_builds.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 docker-compose up -d --build
-docker-compose exec server bash -c "rm -rf build && \
-                                    rm fract4d/*.so"
+docker-compose exec server bash -c "rm -rf build"
 docker-compose exec server bash -c "python3.5 ./setup.py build && \
                                     python3.6 ./setup.py build && \
                                     python3.7 ./setup.py build && \
--- docker/bin/run_pylint.sh
+++ docker/bin/run_pylint.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 docker-compose up -d --build
-docker-compose exec server bash -c "rm -rf build && \
-                                    rm fract4d/*.so"
+docker-compose exec server bash -c "rm -rf build"
 docker-compose exec server bash -c "python3 setup.py build && \
                                     ./bin/pylint.sh"
 docker-compose down
