e1b11f5e59f8ccd803381fae6a07b1b3df49704c
Alberto Gonzalez <mindhells@gmail.com>
2020-05-15 09:45:50 +0100

    fix docker test script

--- docker/bin/run_test.sh
+++ docker/bin/run_test.sh
@@ -3,5 +3,5 @@
 docker-compose up -d --build
 docker-compose exec server bash -c "rm -rf build"
 docker-compose exec server bash -c "python3 setup.py build && \
-                                    ./test.py"
+                                    pytest fract4d fract4dgui fract4d_compiler test.py"
 docker-compose down
