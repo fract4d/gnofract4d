c6c93562f626a0366b1f69e5ff465f46f6fe1715
Edwin Young <edwin@bathysphere.org>
2020-05-24 08:03:56 -0700

    disable frequency scaling during benchmark

--- examples/cpp/mp_benchmark.sh
+++ examples/cpp/mp_benchmark.sh
@@ -4,4 +4,14 @@ docker build . -f examples/cpp/Dockerfile -t mp_mandelbrot:1.0.0 \
     --build-arg main_source=benchmark.cpp \
     --build-arg formula_source=examples/cpp/mp_formula.cpp \
     --build-arg gmp_support=1
+
+# disable CPU frequency scaling during test for hopefully more consistent results
+if [ -x "$(command -v cpupower)" ]; then
+    sudo cpupower frequency-set --governor performance
+fi
+
 docker run --rm -it -v ${PWD}/examples/output:/src/output mp_mandelbrot:1.0.0
+
+if [ -x "$(command -v cpupower)" ]; then
+    sudo cpupower frequency-set --governor powersave
+fi
