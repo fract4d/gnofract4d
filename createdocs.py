0da8655604a88d5e9d2ec40eb8e0ec4daf2c2d39
Edwin Young <edwin@bathysphere.org>
2020-05-31 00:02:17 -0700

    tweak doc generation process

--- createdocs.py
+++ createdocs.py
@@ -10,26 +10,41 @@
 
 import sys
 import os
+import re
+
+def insert_docs(infile,outfile):
+    'look through infile for {{filename}} and insert the contents of filename in output'
+    inputdir = os.path.dirname(infile)
+    insert = re.compile(r'{{(.+)}}')
+    with open(outfile, "w") as output:
+        with open(infile, "r") as input:
+            for line in input:
+                m = insert.match(line)
+                if m:
+                    file_to_insert = os.path.join(inputdir, m.group(1))
+                    with open(file_to_insert, "r") as insertfile:
+                        for l in insertfile:
+                            print(l, file=output,end='')
+                else:
+                    print(line, file=output, end='')
+
 
 def create_stdlib_docs():
     'Autogenerate docs'
     try:
         # create list of stdlib functions
         from fract4d import createdocs as cd1
-        cd1.main("doc/gnofract4d-manual/C/stdlib.xml")
+        cd1.main("doc/stdlib.html")
 
         # create list of mouse and GUI commands
         import fract4dgui.createdocs
-        fract4dgui.createdocs.main("doc/gnofract4d-manual/C/commands.xml")  # pylint: disable=no-value-for-parameter
+        fract4dgui.createdocs.main("doc/commands.html")  # pylint: disable=no-value-for-parameter
 
-        # create HTML version of docs for them as don't have yelp
-        os.chdir("doc/gnofract4d-manual/C")
-        retval = os.system("xsltproc --nonet --output gnofract4d-manual.html --stringparam html.stylesheet docbook.css gnofract4d.xsl gnofract4d-manual.xml")
-        if retval != 0:
-            raise Exception("error processing xslt")
+
     except Exception as err:
         print("Problem creating docs. Online help will be incomplete.", file=sys.stderr)
         print(err, file=sys.stderr)
         sys.exit(1)
 
 create_stdlib_docs()
+insert_docs("doc/manual.md","doc/fuil-manual.md")
