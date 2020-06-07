321c725623df2c7e21435c734b2fd7e3169696da
Edwin Young <edwin@bathysphere.org>
2020-06-07 11:50:24 -0700

    guess which pylint to use

--- bin/pylint.sh
+++ bin/pylint.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-pylint3 --rcfile pylintrc *.py gnofract4d fract4d fract4d_compiler fract4dgui
+if [ -x "$(command -v pylint3)" ]; then
+  PYLINT=pylint3
+else
+  PLYINT=pylint
+fi
+
+$PYLINT --rcfile pylintrc *.py gnofract4d fract4d fract4d_compiler fract4dgui
 if [[ $? > 0 ]]
 then
   exit 1
 fi
 
-pylint3 --rcfile pylintrc_new fract4d/fract4d_new
+$PYLINT --rcfile pylintrc_new fract4d/fract4d_new
 if [[ $? > 0 ]]
 then
   exit 1
