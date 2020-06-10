158e248c369258961be97d05b55a6ff87872739c
Chris Mayo <aklhfex@gmail.com>
2020-06-10 12:19:03 +0100

    Use find_resource() correctly for help
    installdir should be relative to share/gnofract4d

--- fract4dgui/main_window.py
+++ fract4dgui/main_window.py
@@ -1274,7 +1274,7 @@ class MainWindow:
 
         # look locally first to support run-before-install
         local_dir = "manual/public/"
-        install_dir = "../../share/gnofract4d/help/"
+        install_dir = "help"
 
         helpfile = fractconfig.T.find_resource(
             base_help_file, local_dir, install_dir)
