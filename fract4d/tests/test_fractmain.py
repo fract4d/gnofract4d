1315f65468e61753ad1159ac8723975caaf0310a
Edwin Young <edwin@bathysphere.org>
2020-07-11 13:14:48 -0700

    improve test

--- fract4d/tests/test_fractmain.py
+++ fract4d/tests/test_fractmain.py
@@ -14,5 +14,18 @@ class Test(testbase.TestSetup):
     def testBasic(self):
         c = self.userConfig
         fm = fractmain.T(c)
-        options = Arguments().parse_args(["--nogui"])
+        options = Arguments().parse_args(["--save", "foo.png"])
         fm.run(options)
+        self.assertTrue(os.path.exists("foo.png"))
+        os.remove("foo.png")
+
+    def testBuildOnly(self):
+        c = self.userConfig
+        fm = fractmain.T(c)
+        options = Arguments().parse_args(["--buildonly", "test.so"])
+
+        fm.run(options)
+        self.assertTrue(os.path.exists("test.so"))
+        os.remove("test.so")
+        if os.path.exists("test.so.c"):
+            os.remove("test.so.c")
