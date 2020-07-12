e1877310bcc0d89828fe5fbfe7ba0639c0424d24
Chris Mayo <aklhfex@gmail.com>
2020-07-12 17:39:08 +0100

    Remove unsupported Python 3.5 from docker
    Python 3.6 is the minimum since:
    82522d4 ("update min python to 3.6  (#143)", 2020-07-03)

--- docker/bin/run_tox.sh
+++ docker/bin/run_tox.sh
@@ -2,8 +2,7 @@
 
 docker-compose up -d --build
 docker-compose exec server bash -c "rm -rf build"
-docker-compose exec server bash -c "python3.5 ./setup.py build && \
-                                    python3.6 ./setup.py build && \
+docker-compose exec server bash -c "python3.6 ./setup.py build && \
                                     python3.7 ./setup.py build && \
                                     python3.8 ./setup.py build && \
                                     tox"
