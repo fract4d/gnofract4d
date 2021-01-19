fract4d/gnofract4d

commit cdcd87eb6e666327a6a618cd6b405001212b2235

convert options.py to use argparse
Remove two options:
--p --params is a duplicate of PARAMFILE
--printstats isn't used

fract4d/fractal.py
@@ -580,26 +580,26 @@ class T(fctutils.T):
         self.dirtyFormula = True
 
     def apply_options(self,options):
-        if options.basename and options.func:
-            self.set_formula(options.basename,options.func)
+        if options.formula.name and options.formula.func:
+            self.set_formula(options.formula.name, options.formula.func)
             self.reset()
 
-        if options.innername and options.innerfunc:
-            self.set_inner(options.innername, options.innerfunc)
+        if options.inner.name and options.inner.func:
+            self.set_formula(options.inner.name, options.inner.func)
             self.reset()
 
-        if options.outername and options.outerfunc:
-            self.set_outer(options.outername, options.outerfunc)
+        if options.outer.name and options.outer.func:
+            self.set_formula(options.outer.name, options.outer.func)
             self.reset()
 
         if options.maxiter != -1:
             self.set_maxiter(options.maxiter)
 
-        for (num,val) in list(options.paramchanges.items()):
-            self.set_param(num,val)
+        for num, val in options.paramchanges.items():
+            self.set_param(num, val)
 
-        for (file,func) in options.transforms:
-            self.append_transform(file,func)
+        for t in options.transforms:
+            self.append_transform(t.name, t.func)
             
         if options.map:
             self.set_cmap(options.map)

fract4d/options.py
@@ -1,233 +1,165 @@
-import getopt # FIXME now we've dropped 2.2 we can use optparse
-import os
-import string
-
-from . import fractal
+import argparse
+import collections
+import os.path
+import sys
 
 # version of Gnofract 4D
-version = '3.14.1'
+VERSION = '3.14.1'
 
-class OptionError(Exception):
-    pass
+POSITION_ARGUMENTS = ("xcenter", "ycenter", "zcenter", "wcenter",
+    "xyangle", "xzangle", "xwangle", "yzangle", "ywangle", "zwangle",
+    "magnitude")
 
-class T:
-    "Gathers command-line args, wraps crappy getOpt interface"
-    longparams = [
-            "params=",
-            "maxiter=",
-            "width=",
-            "height=",
-            "antialias=",
-            "save=",
-            "help",
-            "path=",
-            "formula=",            
-            "inner=",
-            "outer=",
-            "transforms=",
-            "map=",
-            "xcenter=","ycenter=","zcenter=","wcenter=",
-            "xyangle=","xzangle=","xwangle=",
-            "yzangle=","ywangle=","zwangle=",
-            "magnitude=",
-            "quit",
-            "explorer",
-            "trace",
-            "tracez",
-            "nogui",
-            "buildonly=",
-            "usebuilt=",
-            "cflags=",
-            "printstats",
-            "version",
-            "singlepoint",
-            "threads=",
-            "nopreview"]
+Formula = collections.namedtuple('Formula', ['path', 'name', 'func'])
 
-    def __init__(self):
-        (self.basename,self.func) = (None,None)
-        (self.innername,self.innerfunc) = (None,None)
-        (self.outername,self.outerfunc) = (None,None)
-        self.transforms = []
-        self.maxiter = -1
-        self.trace = False
-        self.tracez = False
-        self.buildonly = None
-        self.usebuilt = None
-        self.flags = None
-        self.printstats = False
-        self.singlepoint = False
-        self.print_version = False
-        self.paramchanges = {}
-        self.quit_now = False
-        self.quit_when_done = False
-        self.output = ""
-        self.explore = False
-        self.save_filename = None
-        self.extra_paths = []
-        self.map = None
-        self.nogui = False
-        self.antialias = None
-        self.threads = None
-        (self.width, self.height) = (0,0)
-        self.preview = True
-
-    def help(self):
-        return """Gnofract 4D %s
-Usage: gnofract4d [flags] [paramfile]
-
-To generate an image non-interactively, use:
-gnofract4d -s myimage.png -q myfractal.fct 
-
-General Flags:
--h --help\t\tShow this message
--v --version\t\tShow version info
--q --quit\t\tExit as soon as the image is finished
--X --explorer\t\tStart in explorer mode
-
-Fractal Settings:
--p --params FILE\tUse FILE as a parameter file
--P --path P\t\tAdd P to the formula search path
--f --formula F#func\tUse formula 'func' from file F
-   --inner F#func\tUse coloring algorithm 'func' from file F
-   --outer F#func\tUse coloring algorithm 'func' from file F
-   --transforms F#func1,F2#func2\tApply transforms 'func1' and 'func2'
--m --maxiter N\t\tUse N as maximum number of iterations
-   --map FILE\t\tLoad map file FILE
-
-Output Settings:
--s --save IMAGEFILE\tSave image to IMAGEFILE after calculation
--i --width N\t\tMake image N pixels wide
--j --height N\t\tMake image N pixels tall
--a --antialias MODE\tAntialiasing mode (one of none|fast|best)
-
-Positional Parameters:
-   --xcenter N
-   --ycenter N
-   --zcenter N
-   --wcenter N
-   --xyangle N
-   --xzangle N
-   --xwangle N
-   --yzangle N
-   --ywangle N
-   --zwangle N
-   --magnitude N
-
-Obscure settings:
-   --nogui\t\tRun with no UI (doesn't require X or GTK)
-   --threads N\t\tUse N threads for calculations
-   --nopreview\t\tUse the UI, but no preview window
-
-Debugging and Profiling settings (most only work with --nogui):
-   --trace\t\tProduce voluminous tracing output
-   --tracez\t\tPrint values of #z as loop runs
-   --printstats\tPrint timing information after each image
-   --buildonly FILE\tGenerate code to FILE and quit
-   --usebuilt FILE\tInstead of using compiler, load FILE (from buildonly)
-   --singlepoint\tGenerate only a single point many times over (for benchmarking)
-   --cflags FLAGS\tPass these flags to C compiler (overrides prefs)
-
-""" % version
-
-    def parse(self,args):
-        (opts, self.args) = (None,None)
-        try:
-            (opts, self.args) = getopt.getopt(
-                args,
-                "p:m:i:j:a:s:f:qhP:Xv", T.longparams)
-        except getopt.GetoptError as err:
-            raise OptionError(err)
+def formula_type_parse(name, string):
+        n = string.rfind('#')
+        if n == -1:
+            raise argparse.ArgumentTypeError(
+                "argument '%s' to %s should be file#func" % (string, name))
         
-        for (name, val) in opts:
-            if name=="-p" or name=="--params":
-                self.args.insert(0,val)
-            elif name=="-m" or name=="--maxiter":
-                self.maxiter=int(val)
-            elif name=="-q" or name=="--quit":
-                self.quit_when_done = True
-            elif name=="-s" or name=="--save":
-                self.save_filename = val
-            elif name=="-i" or name=="--width":
-                self.width = int(val)
-            elif name=="-j" or name=="--height":
-                self.height = int(val)
-            elif name=="-a" or name=="--antialias":
-                self.antialias = self.parse_aa(val)
-            elif name=="-h" or name=="--help":
-                self.output += self.help()
-                self.quit_now = True
-            elif name=="-P" or name=="--path":
-                self.extra_paths.append(val)
-            elif name=="-X" or name=="--explorer":
-                self.explore = True
-            elif name=="-f" or name=="--formula":
-                (self.basename,self.func) = self.splitarg(val,name)
-            elif name=="--inner":
-                (self.innername,self.innerfunc) = self.splitarg(val,name)
-            elif name=="--outer":
-                (self.outername,self.outerfunc) = self.splitarg(val,name)
-            elif name=="--transforms":
-                tlist = val.split(",")
-                for t in tlist:
-                    self.transforms.append(self.splitarg(t,name))
-            elif (name=="--map"):
-                self.map = val
-            elif name=="--version" or name=="-v":
-                self.print_version = True
-                self.quit_now = True
-            elif name=="--trace":
-                self.trace = True
-            elif name=="--tracez":
-                self.tracez = True
-            elif name=="--buildonly":
-                self.buildonly = val
-            elif name=="--usebuilt":
-                self.usebuilt = val
-            elif name=="--cflags":
-                self.flags = val
-            elif name=="--nogui":
-                self.nogui = True
-            elif name=="--nopreview":
-                self.preview = False
-            elif name=="--threads":
-                self.threads = int(val)
-            elif name=="--printstats":
-                self.printstats = True
-            elif name=="--singlepoint":
-                self.singlepoint = True
-            else:
-                # see if it's a positional param
-                pname = name[2:].upper()
-                if hasattr(fractal.T,pname):
-                    pnum = getattr(fractal.T,pname)
-                    self.paramchanges[pnum]= val
-                else:
-                    self.output += "Unknown argument: '%s' with value '%s'" % (name, val)
-
-    def splitarg(self,val, name):
-        n = val.rfind('#')
-        if n==-1:            
-            raise OptionError(
-                "argument '%s' to %s should be file#func" % (val,name))
-        
-        (file, func) = (val[:n], val[n+1:])
+        file, func = string[:n], string[n+1:]
         path = os.path.dirname(file)
-        if path:
-            self.extra_paths.append(path)
-
         basename = os.path.basename(file)
-        return (basename,func)
+        
+        f = Formula(path, basename, func)
+        return f
 
-    def parse_aa(self,val):
-        val = val.lower()
-        if val == "none":
-            return 0
-        if val == "fast":
-            return 1
-        if val == "best":
-            return 2
-        raise OptionError(
-            "antialias option must be one of: 'none', 'fast', 'best', not '%s'" % val)
-            
+def formula_arg(string):
+    return formula_type_parse("--formula", string)
+    
+def inner_arg(string):
+    return formula_type_parse("--inner", string)
 
+def outer_arg(string):
+    return formula_type_parse("--outer", string)
+
+def transforms_arg(string):
+    transforms = []
+    tlist = string.split(",")
+    for t in tlist:
+        transforms.append(formula_type_parse("--transforms", t))
+    return transforms
+
+class Arguments(argparse.ArgumentParser):
+    def __init__(self):
+        argparse.ArgumentParser.__init__(self,
+            description="Gnofract 4D %s" % VERSION,
+            epilog="To generate an image non-interactively, use:\n"
+                    "  gnofract4d -s myimage.png -q myfractal.fct",
+            formatter_class=argparse.RawDescriptionHelpFormatter
+            )
+
+        self.add_argument("paramfile", nargs='?', metavar="PARAMFILE",
+            help="Use PARAMFILE as a parameter file")
+
+        self.add_argument("-v", "--version", action="version",
+            version="Gnofract 4D %s" % VERSION, help="Show version info")
+
+        self.add_argument("-q", "--quit", action="store_true",
+            dest="quit_when_done", help="Exit as soon as the image is finished")
+
+        self.add_argument("-X", "--explorer", action="store_true",
+            dest="explore", help="Start in explorer mode")
+
+        fractal = self.add_argument_group("Fractal Settings")
+        output = self.add_argument_group("Output Settings")
+        position = self.add_argument_group("Position Arguments")
+        obscure = self.add_argument_group("Obscure Settings")
+        debug = self.add_argument_group(
+            "Debugging and Profiling Settings (most only work with --nogui)")
+        
+        fractal.add_argument("-P", "--path",
+            metavar="PATH", nargs="*", dest="extra_paths", default=[],
+            help="Add PATH to the formula search path")
+
+        fractal.add_argument("-f", "--formula", type=formula_arg,
+            metavar="F#FUNC", default=Formula(None, None, None),
+            help="Use formula 'FUNC' from file F")
+
+        fractal.add_argument("--inner", type=inner_arg,
+            metavar="F#FUNC", default=Formula(None, None, None),
+            help="Use coloring algorithm 'FUNC' from file F")
+
+        fractal.add_argument("--outer", type=outer_arg,
+            metavar="F#FUNC", default=Formula(None, None, None),
+            help="Use coloring algorithm 'FUNC' from file F")
+
+        fractal.add_argument("--transforms", type=transforms_arg,
+            metavar="F#FUNC1,F2#FUNC2", default=[],
+            help="Apply transforms 'FUNC1' and 'FUNC2'")
+
+        fractal.add_argument("-m", "--maxiter", type=int,
+            metavar="N", default=-1,
+            help="Use N as maximum number of iterations")
+
+        fractal.add_argument("--map", metavar="FILE",
+            help="Load map file FILE")
+
+        output.add_argument("-s", "--save", metavar="IMAGEFILE", dest="save_filename",
+            help="Save image to IMAGEFILE after calculation")
+
+        output.add_argument("-i", "--width", type=int, metavar="N",
+            help="Make image N pixels wide")
+
+        output.add_argument("-j", "--height", type=int, metavar="N",
+            help="Make image N pixels tall")
+
+        output.add_argument("--antialias", metavar="MODE",
+            choices=['none', 'fast', 'best'],
+            help="Antialiasing MODE (one of none|fast|best)")
+
+        for p in POSITION_ARGUMENTS:
+            position.add_argument("--%s" % p, type=int, metavar="N")
+
+        obscure.add_argument("--nogui", action="store_true",
+            help="Run with no UI (doesn't require X or GTK+)")
+
+        obscure.add_argument("--threads", type=int, metavar="N",
+            help="Use N threads for calculations")
+
+        obscure.add_argument("--nopreview", action="store_false",
+            dest="preview", help="Use the UI, but no preview window")
+
+        debug.add_argument("--trace", action="store_true",
+            help="Produce voluminous tracing output")
+
+        debug.add_argument("--tracez", action="store_true",
+            help="Print values of #z as loop runs")
+
+        #debug.add_argument("--printstats", action="store_true",
+        #    help="Print timing information after each image")
+
+        debug.add_argument("--buildonly", metavar="FILE",
+            help="Generate code to FILE and quit")
+
+        debug.add_argument("--usebuilt", metavar="FILE",
+            help="Instead of using compiler, load FILE (from buildonly)")
+
+        debug.add_argument("--singlepoint", action="store_true",
+            help="Generate only a single point many times over (for benchmarking)")
+
+        debug.add_argument("--cflags", dest="flags",
+            help="Pass these FLAGS to C compiler (overrides prefs)")
+
+    def parse_args(self, args=sys.argv):
+        opts = argparse.ArgumentParser.parse_args(self, args)
+
+        if opts.formula.path:
+            opts.extra_paths.append(opts.formula.path)
+        if opts.inner.path:
+            opts.extra_paths.append(opts.inner.path)
+        if opts.outer.path:
+            opts.extra_paths.append(opts.outer.path)
+        for t in opts.transforms:
+            if t.path:
+                opts.extra_paths.append(t.path)
+
+        opts.paramchanges = {}
+        for a in POSITION_ARGUMENTS:
+            val = getattr(opts, a)
+            if val is not None:
+                pnum = getattr(fractal.T, a.upper())
+                opts.paramchanges[pnum] = val
+
+        return opts

fract4d/fractmain.py
@@ -25,8 +25,8 @@ class T:
         threads = options.threads or self.userConfig.getint(
             "general","threads")
 
-        if len(options.args) > 0:
-            self.load(options.args[0])
+        if options.paramfile:
+            self.load(options.paramfile)
 
         self.f.apply_options(options)
         self.f.antialias = options.antialias or \

fract4d/test_options.py
@@ -1,83 +1,72 @@
 #!/usr/bin/env python3
+
 import unittest
 import sys
 
 if sys.path[1] != "..": sys.path.insert(1, "..")
 
-from fract4d import options
+from fract4d.options import Arguments, Formula
 
 class Test(unittest.TestCase):
     def testDefaults(self):
-        o = options.T()
+        o = Arguments()
+        ns = o.parse_args()
         
-        for x in [o.basename, o.func,
-                  o.innername, o.innerfunc, o.outername, o.outerfunc, o.map]:
+        for x in [ns.formula.name, ns.formula.func,
+                  ns.inner.name, ns.inner.func, ns.outer.name, ns.outer.func, ns.map]:
             self.assertEqual(None,x)
         
-        for flag in [o.trace, o.tracez, o.print_version,
-                     o.quit_now, o.quit_when_done,
-                     o.explore, o.nogui]:
+        for flag in [ns.trace, ns.tracez, ns.quit_when_done,
+                     ns.explore, ns.nogui]:
             self.assertEqual(False, flag)
             
-        self.assertEqual(-1,o.maxiter)
-        self.assertEqual({},o.paramchanges)
-        self.assertEqual("",o.output)
-        self.assertEqual([],o.extra_paths)
+        self.assertEqual(-1,ns.maxiter)
+        self.assertEqual({},ns.paramchanges)
+        self.assertEqual([],ns.extra_paths)
 
-        self.assertEqual(0,o.width)
-        self.assertEqual(0,o.height)
+        self.assertEqual(None,ns.width)
+        self.assertEqual(None,ns.height)
 
     def testBadOption(self):
-        o = options.T()
-        self.assertRaises(options.OptionError, o.parse,["--fish"])
+        o = Arguments()
+        self.assertRaises(SystemExit, o.parse_args, ["--fish"])
 
     def testHelp(self):
-        o = options.T()
-        o.parse(["-h"])
-        self.assertEqual(1, o.output.count("To generate an image"))
-        self.assertEqual(True, o.quit_now)
+        o = Arguments()
+        help = o.format_help()
+        self.assertEqual(1, help.count("To generate an image"))
 
     def testGeneralOptions(self):
-        o = options.T()
-        o.parse(["-P", "foo", "-f", "bar/baz.frm#wibble"])
-
-        self.assertEqual(["foo","bar"],o.extra_paths)
-        self.assertEqual("baz.frm",o.basename)
-        self.assertEqual("wibble",o.func)
+        o = Arguments()
+        ns = o.parse_args(["-P", "foo", "bar", "-f", "baz/baz.frm#wibble"])
+        self.assertEqual(["foo", "bar", "baz"],ns.extra_paths)
+        self.assertEqual("baz.frm",ns.formula.name)
+        self.assertEqual("wibble",ns.formula.func)
 
     def testBadSplit(self):
-        o = options.T()
-        self.assertRaises(options.OptionError,o.parse,["-f", "bar"])
+        o = Arguments()
+        self.assertRaises(SystemExit, o.parse_args, ["-f", "bar"])
         
     def testHeightWidth(self):
-        o = options.T()
-        o.parse(["-i", "780", "-j", "445"])
+        o = Arguments()
+        ns = o.parse_args(["-i", "780", "-j", "445"])
 
-        self.assertEqual(780, o.width)
-        self.assertEqual(445, o.height)
+        self.assertEqual(780, ns.width)
+        self.assertEqual(445, ns.height)
 
     def testArgument(self):
-        o = options.T()
-        o.parse(["--params", "foo"])
+        o = Arguments()
+        ns = o.parse_args(["foo"])
 
-        self.assertEqual(["foo"], o.args)
+        self.assertEqual("foo", ns.paramfile)
 
     def testTransforms(self):
-        o = options.T()
-        o.parse(["--transforms", "a#b,x#y"])
-        self.assertEqual([("a","b"), ("x","y")], o.transforms)
-        
-    def testAllOptionsHaveHelp(self):
-        o = options.T()
-        help = o.help()
+        o = Arguments()
+        ns = o.parse_args(["--transforms", "a#b,x#y"])
+        self.assertEqual([Formula("", "a","b"), Formula("", "x","y")], ns.transforms)
 
-        names = ["--" + x.rstrip("=") for x in options.T.longparams]
-        for name in names:
-            self.assertNotEqual(0, help.count(name), "%s has no help" % name)
-            
 def suite():
     return unittest.makeSuite(Test,'test')
 
 if __name__ == '__main__':
     unittest.main(defaultTest='suite')
-

fract4dgui/main_window.py
@@ -1411,8 +1411,8 @@ class MainWindow:
         for path in opts.extra_paths:
             self.compiler.add_func_path(path)
 
-        if len(opts.args) > 0:
-            self.load(opts.args[0])
+        if opts.paramfile:
+            self.load(opts.paramfile)
 
         self.f.apply_options(opts)
         self.update_preview(self.f)

fract4dgui/test_main_window.py
@@ -16,13 +16,13 @@ gettext.install('gnofract4d')
 
 if sys.path[1] != "..": sys.path.insert(1, "..")
 
-from fract4d import fractal
+from fract4d import fractal, fractconfig
 from fract4dgui import main_window
 
 class WrapMainWindow(main_window.MainWindow):
     def __init__(self):
         self.errors = []
-        main_window.MainWindow.__init__(self, ['../formulas'])
+        main_window.MainWindow.__init__(self, fractconfig.T(""), ['../formulas'])
 
     def show_error_message(self,message,exception):
         self.errors.append((message,exception))

gnofract4d
@@ -16,7 +16,8 @@ else:
 
 try:
     # nogui modules
-    from fract4d import fractconfig, fractal, fractmain, options
+    from fract4d import fractconfig, fractal, fractmain
+    from fract4d.options import Arguments
 
 except ImportError as err:
     print((_('''
@@ -25,43 +26,12 @@ If you haven't set up Gnofract 4D yet, run './setup.py build'
 Error was: '%s' ''') % err))
     sys.exit(1)
 
-def get_version_info():
-    try:
-        from gi.repository import Gtk
-        gtk_version = "%d.%d.%d" % (Gtk.get_major_version(), Gtk.get_minor_version(), Gtk.get_micro_version())
-    except:
-        gtk_version = "unknown"
-
-    return """Gnofract 4D %s
-GTK %s""" % (options.version, gtk_version)
-               
-def main(args):    
+def main(args):
     # saved options
     userConfig = fractconfig.userConfig()
 
     # command line options
-    opts = options.T()
-    try:
-        opts.parse(args)
-    except options.OptionError as err:
-        print(get_version_info())
-        print(opts.help())
-
-        print("Error parsing arguments: %s" % err)
-        return 1
-
-    # having parsed them, apply them. The reason for this 2-pass
-    # approach is that it matters what order these operations are
-    # done in, but it shouldn't matter what order params are
-    # presented on the command-line
-    if len(opts.output) > 0:
-        print(opts.output)
-
-    if opts.print_version:
-        print(get_version_info())
-        
-    if opts.quit_now:
-        sys.exit(0)
+    opts = Arguments().parse_args(args)
 
     try:
         if opts.nogui:

test.py
@@ -25,7 +25,7 @@ class Test(unittest.TestCase):
         m = doc_re.search(doc)
 
         self.assertTrue(m,"setup.py doesn't specify version")
-        self.assertEqual(options.version, m.group(1))
+        self.assertEqual(options.VERSION, m.group(1))
 
     def testDocVersionMatches(self):        
         # check the docs
@@ -34,7 +34,7 @@ class Test(unittest.TestCase):
 
         m = doc_re.search(doc)
         self.assertTrue(m,"doc doesn't specify version")
-        self.assertEqual(options.version,m.group(1), "Version mismatch")
+        self.assertEqual(options.VERSION,m.group(1), "Version mismatch")
 
     def testWebsiteVersionMatches(self):
         if not os.path.exists("website"):
@@ -45,7 +45,7 @@ class Test(unittest.TestCase):
 
         m = ver_re.search(mkweb)
         self.assertTrue(m,"doc doesn't specify version")
-        self.assertEqual(options.version,m.group(1), "Version mismatch")
+        self.assertEqual(options.VERSION,m.group(1), "Version mismatch")
 
     def testGenerateMandelbrot(self):
         if os.path.exists("test.png"):

commit 748e6ce4f5f1e3ba0636699fe8c6be48e620608d

Fix --antialias segfaults
Broken in:
cdcd87e ("convert options.py to use argparse", 2018-03-04)

fract4d/fractal.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import copy
+import enum
 import io
 import math
 import random
@@ -17,6 +18,12 @@ BLEND_NEAREST = 0
 BLEND_FURTHEST = 1
 BLEND_CW = 2
 BLEND_CCW = 3
+
+
+class AntialiasModes(enum.IntEnum):
+    none = 0
+    fast = 1
+    best = 2
 
 
 class T(fctutils.T):
@@ -72,7 +79,7 @@ class T(fctutils.T):
         self.auto_epsilon = False  # automatically set @epsilon param, if found
         self.auto_deepen = True  # automatically adjust maxiter
         self.auto_tolerance = True  # automatically adjust periodicity
-        self.antialias = 1
+        self.antialias = AntialiasModes.fast.value
         self.compiler = compiler
         self.outputfile = None
         self.render_type = 0
@@ -603,7 +610,7 @@ class T(fctutils.T):
             self.set_cmap(options.map)
 
         if options.antialias is not None:
-            self.antialias = options.antialias
+            self.antialias = AntialiasModes[options.antialias].value
 
     def compile(self):
         if self.forms[0].formula is None:

fract4d/options.py
@@ -2,7 +2,7 @@ import argparse
 import collections
 import os.path
 import sys
-from . import fractal
+from . import fractal as fract4d_fractal
 
 # version of Gnofract 4D
 VERSION = '4.3'
@@ -12,7 +12,6 @@ POSITION_ARGUMENTS = ("xcenter", "ycenter", "zcenter", "wcenter",
                       "magnitude")
 
 Formula = collections.namedtuple('Formula', ['path', 'name', 'func'])
-
 
 def formula_type_parse(name, string):
     n = string.rfind('#')
@@ -112,9 +111,10 @@ class Arguments(argparse.ArgumentParser):
         output.add_argument("-j", "--height", type=int, metavar="N",
                             help="Make image N pixels tall")
 
+        aa_modes = [i.name for i in fract4d_fractal.AntialiasModes]
         output.add_argument("--antialias", metavar="MODE",
-                            choices=['none', 'fast', 'best'],
-                            help="Antialiasing MODE (one of none|fast|best)")
+                            choices=aa_modes,
+                            help=f"Antialiasing MODE (one of {'|'.join(aa_modes)})")
 
         for p in POSITION_ARGUMENTS:
             position.add_argument("--%s" % p, type=int, metavar="N")
@@ -166,7 +166,7 @@ class Arguments(argparse.ArgumentParser):
         for a in POSITION_ARGUMENTS:
             val = getattr(opts, a)
             if val is not None:
-                pnum = getattr(fractal.T, a.upper())
+                pnum = getattr(fract4d_fractal.T, a.upper())
                 opts.paramchanges[pnum] = val
 
         return opts

fract4d/tests/test_fractal.py
@@ -12,6 +12,7 @@ from . import testbase
 
 from fract4d_compiler import fc, fracttypes
 from fract4d import fractal, image, formsettings
+from fract4d.options import Arguments
 
 g_testfile = '''gnofract4d parameter file
 version=2.0
@@ -1654,6 +1655,13 @@ solids=[
         blend = f.blend(f2, 0.5)
 
         self.assertEqual(math.pi / 8.0, blend.get_param(f.XYANGLE))
+
+    def testAntialiasArgument(self):
+        o = Arguments()
+        ns = o.parse_args(["--antialias", "best"])
+        f = fractal.T(Test.g_comp)
+        f.apply_options(ns)
+        self.assertEqual(f.antialias, 2)
 
     def testSetCompilerOptions(self):
         f = fractal.T(Test.g_comp)