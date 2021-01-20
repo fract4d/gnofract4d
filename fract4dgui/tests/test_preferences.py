312df1da5c2251e0bea82cbab34a70d0c706d9ba
Chris Mayo <aklhfex@gmail.com>
2021-01-20 19:36:25 +0000

    Add test for PrefsDialog

--- fract4dgui/tests/test_preferences.py
+++ fract4dgui/tests/test_preferences.py
@@ -2,12 +2,17 @@
 
 # test classes for preferences logic
 
+from . import testgui
+
 from fract4d import fractconfig
-from fract4dgui import preferences
-import unittest
+from fract4dgui import gtkfractal, preferences
+
+import gettext
+gettext.install('gnofract4d')
 
 import gi
 gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
 
 
 class CallCounter:
@@ -18,31 +23,33 @@ class CallCounter:
         self.count += 1
 
 
-class Test(unittest.TestCase):
-    def setUp(self):
-        baseconfig = fractconfig.T("test.config")
-        self.config = preferences.Preferences(baseconfig)
-
-    def tearDown(self):
-        pass
-
+class Test(testgui.TestCase):
     def testSignals(self):
+        baseconfig = fractconfig.T("test.config")
+        config = preferences.Preferences(baseconfig)
+
         counter = CallCounter()
 
-        self.config.connect('preferences-changed', counter.cb)
+        config.connect('preferences-changed', counter.cb)
 
         # callback should happen
-        self.config.set('compiler', 'name', 'cc')
+        config.set('compiler', 'name', 'cc')
         self.assertEqual(counter.count, 1)
 
         # no callback, value already set
-        self.config.set('compiler', 'name', 'cc')
+        config.set('compiler', 'name', 'cc')
         self.assertEqual(counter.count, 1)
 
         # new option, callback called
-        self.config.set('compiler', 'foop', 'cc')
+        config.set('compiler', 'foop', 'cc')
         self.assertEqual(counter.count, 2)
 
     def testInstance(self):
         dummy = preferences.Preferences(fractconfig.T(""))
         self.assertNotEqual(None, dummy)
+
+    def testPrefsDialog(self):
+        parent = Gtk.Window()
+        f = gtkfractal.T(Test.g_comp, parent)
+        pd = preferences.PrefsDialog(parent, f, preferences.Preferences(Test.userConfig))
+        self.assertEqual(pd.f, f)
