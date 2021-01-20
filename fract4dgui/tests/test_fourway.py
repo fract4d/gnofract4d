724773a0784794d613060b9b8ada032d56050db0
Chris Mayo <aklhfex@gmail.com>
2021-01-20 19:36:25 +0000

    Add tests for angle, fourway and main_window

--- fract4dgui/tests/test_fourway.py
+++ fract4dgui/tests/test_fourway.py
@@ -3,11 +3,12 @@
 # unit tests for model
 
 from fract4dgui import fourway
-from gi.repository import Gtk
+
 import unittest
 
 import gi
 gi.require_version('Gtk', '3.0')
+from gi.repository import Gdk, Gtk
 
 
 class EmitCounter:
@@ -42,3 +43,24 @@ class Test(unittest.TestCase):
         w.add(f)
         w.show()
         Gtk.main_iteration()
+
+    def testMouseButton1(self):
+        f = fourway.T("button1")
+        event = Gdk.EventButton()
+        event.button = 1
+        event.x, event.y = 1, 1
+        widget = Gtk.Window()
+        rectangle = Gdk.Rectangle()
+        rectangle.width = 100
+        rectangle.height = 100
+        widget.size_allocate(rectangle)
+
+        f.onButtonPress(widget, event)
+        self.assertEqual(f.last_x, 1)
+
+        event.x = 2
+        f.onMotionNotify(widget, event)
+        self.assertEqual(f.last_x, 2)
+
+        f.onButtonRelease(widget, event)
+        self.assertFalse(f.notice_mouse)
