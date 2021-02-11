5855003e119e568f77d125fdc92c913ace73ac8f
Chris Mayo <aklhfex@gmail.com>
2021-02-11 19:07:33 +0000

    Update copyright to 2021 (#224)

--- fract4dgui/application_window.py
+++ fract4dgui/application_window.py
@@ -163,7 +163,7 @@ class ApplicationDialogs:
             modal=True,
             comments=_("Easy to use fractal image generator supporting multiple"
                        " views of a four‑dimensional object"),
-            copyright=_("Copyright © 1999-2020, Edwin Young\n"
+            copyright=_("Copyright © 1999-2021, Edwin Young\n"
                         "All rights reserved.\n"
                         "Distributed under the BSD license."
                         ' See the file "LICENSE" for details.'),
