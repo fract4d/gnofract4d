bc6be40aab162dc9121b87daa7c98a1d50933492
Chris Mayo <aklhfex@gmail.com>
2021-02-11 19:11:32 +0000

    Combine director dialogs

--- fract4dgui/director_dialogs.py
+++ fract4dgui/director_dialogs.py
@@ -114,3 +114,45 @@ class DirectorPrefs:
             self.animation.set_png_dir(self.txt_temp_png.get_text())
 
         self.dialog.destroy()
+
+
+class DlgAdvOptions:
+    # Original copyright from DlgAdvOpt.py:
+    # Copyright (C) 2006  Branko Kokanovic
+    """Dialog for advanced interpolation options for director"""
+    def __init__(self, current_kf, animation, parent):
+        self.dialog = Gtk.Dialog(
+            transient_for=parent,
+            title="Keyframe advanced options",
+            modal=True,
+            destroy_with_parent=True
+        )
+        self.dialog.add_buttons(_("_OK"), Gtk.ResponseType.OK,
+                                _("_Cancel"), Gtk.ResponseType.CANCEL)
+
+        self.current_kf = current_kf
+        self.animation = animation
+
+        tbl_main = Gtk.Grid(row_spacing=10, column_spacing=10, margin=10)
+        dirs = animation.get_directions(self.current_kf)
+        self.cmbs = []
+        for i, plane in enumerate(["XY", "XZ", "XW", "YZ", "YW", "ZW"]):
+            tbl_main.attach(
+                Gtk.Label(label=f"{plane} angles interpolation direction:"), 0, i, 1, 1)
+            cmb = utils.combo_box_text_with_items(
+                ["Nearer", "Longer", "Clockwise", "Counterclockwise"])
+            cmb.set_active(dirs[i])
+            tbl_main.attach(cmb, 1, i, 1, 1)
+            self.cmbs.append(cmb)
+
+        self.dialog.vbox.pack_start(tbl_main, True, True, 0)
+
+    def show(self):
+        self.dialog.show_all()
+        response = self.dialog.run()
+        if response == Gtk.ResponseType.OK:
+            dirs = ([cmb.get_active() for cmb in self.cmbs])
+            self.animation.set_directions(self.current_kf, dirs)
+
+        self.dialog.destroy()
+        return
--- fract4dgui/tests/test_director_dialogs.py
+++ fract4dgui/tests/test_director_dialogs.py
@@ -8,7 +8,7 @@ from . import testgui
 
 from gi.repository import Gtk
 
-from fract4dgui import director_prefs
+from fract4dgui import director_dialogs
 from fract4d import fractal, animation
 
 
@@ -19,7 +19,7 @@ class Test(testgui.TestCase):
 
     def testDirectorPrefsDialog(self):
         parent = Gtk.Window()
-        dp = director_prefs.DirectorPrefs(self.test_animation, parent)
+        dp = director_dialogs.DirectorPrefs(self.test_animation, parent)
 
         self.assertEqual(dp.chk_create_fct.get_label(), "Create temporary .fct files")
 
@@ -30,10 +30,21 @@ class Test(testgui.TestCase):
         mock_dialog_get_filename.side_effect = lambda: filename
 
         parent = Gtk.Window()
-        dp = director_prefs.DirectorPrefs(self.test_animation, parent)
+        dp = director_dialogs.DirectorPrefs(self.test_animation, parent)
 
         mock_dialog_run.side_effect = lambda: Gtk.ResponseType.OK
         self.assertEqual(dp.get_folder(), filename)
 
         mock_dialog_run.side_effect = lambda: Gtk.ResponseType.CANCEL
         self.assertEqual(dp.get_folder(), "")
+
+    @patch("gi.repository.Gtk.Dialog.run")
+    def testDlgAdvOptions(self, mock_dialog_run):
+        mock_dialog_run.side_effect = lambda: Gtk.ResponseType.OK
+
+        parent = Gtk.Window()
+        self.test_animation.add_keyframe(
+            "testdata/director1.fct", 1, 10, animation.INT_LOG)
+        dao = director_dialogs.DlgAdvOptions(0, self.test_animation, parent)
+        dao.show()
+        self.assertTrue(dao.animation.keyframes[0].flags)
