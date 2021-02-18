f8c24e8819f13151c49f85db71015f37bf1c2754
Chris Mayo <aklhfex@gmail.com>
2021-02-18 19:24:12 +0000

    Use toolbar CSS

--- fract4dgui/application_widgets.py
+++ fract4dgui/application_widgets.py
@@ -169,17 +169,17 @@ class FractalFilename:
 
 class Toolbar(Gtk.Box):
     def __init__(self):
-        super().__init__(margin=5, spacing=1)
+        super().__init__()
+        self.get_style_context().add_class("toolbar")
 
     @staticmethod
     def button_args(icon_name, tip_text, action):
-        icon = Gtk.Image(icon_name=icon_name, icon_size=Gtk.IconSize.LARGE_TOOLBAR, margin=5)
+        icon = Gtk.Image(icon_name=icon_name, icon_size=Gtk.IconSize.LARGE_TOOLBAR)
         return dict(
             action_name=action, image=icon, relief=Gtk.ReliefStyle.NONE, tooltip_text=tip_text)
 
     def add_space(self):
-        self.add(Gtk.Separator(
-            orientation=Gtk.Orientation.VERTICAL, margin_start=5, margin_end=5))
+        self.add(Gtk.Separator(orientation=Gtk.Orientation.VERTICAL))
 
     def add_button(self, icon_name, tip_text, action):
         self.add(Gtk.Button(**self.button_args(icon_name, tip_text, action)))
