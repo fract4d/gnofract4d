fract4d/gnofract4d

commit 3e3509e85813e0f942cdbaab1cfacc37cce6602b

Fix duplicating formula_paths in user configuration (#217)
If the user removes _shared_formula_dir, on the next start-up
a second ${HOME}/gnofract4d/formulas was also being added.

Fixed by removing the numeric keys from the path items - order is
preserved by the dictionary containing them.

fract4d/fractconfig.py
@@ -68,19 +68,19 @@ class T(configparser.ConfigParser):
              OrderedDict()
              ),
             ("formula_path",
-             OrderedDict((
-                         ("0", "formulas"),
-                         ("1", _shared_formula_dir),
-                         ("2", os.path.expandvars(
-                             "${HOME}/gnofract4d/formulas")),
+             OrderedDict.fromkeys((
+                         "formulas",
+                         _shared_formula_dir,
+                         os.path.expandvars(
+                             "${HOME}/gnofract4d/formulas"),
                          ))
              ),
             ("map_path",
-             OrderedDict((
-                         ("0", "maps"),
-                         ("1", _shared_map_dir),
-                         ("2", os.path.expandvars("${HOME}/gnofract4d/maps")),
-                         ("3", "/usr/share/gimp/2.0/gradients"),
+             OrderedDict.fromkeys((
+                         "maps",
+                         _shared_map_dir,
+                         os.path.expandvars("${HOME}/gnofract4d/maps"),
+                         "/usr/share/gimp/2.0/gradients",
                          ))
              ),
             ("recent_files",
@@ -103,13 +103,24 @@ class T(configparser.ConfigParser):
             "compiler": True
         }
 
-        configparser.ConfigParser.__init__(self, interpolation=None)
+        configparser.ConfigParser.__init__(self, allow_no_value=True, interpolation=None)
         self.read_dict(_defaults)
         self.file = os.path.expanduser(file)
         self.read(self.file)
+        self.update_paths("formula_path")
+        self.update_paths("map_path")
 
         self.ensure_contains("formula_path", _shared_formula_dir)
         self.ensure_contains("map_path", _shared_map_dir)
+
+    def update_paths(self, section):
+        # Gnofract 4D 4.3 and earlier configuration used index numbers with
+        # formula and map path entries. This method converts such entries to the
+        # current format. It can be removed in future.
+        for key, value in self[section].items():
+            if value is not None:
+                self[section][value] = None
+                del self[section][key]
 
     def ensure_contains(self, section, required_item):
         l = self.get_list(section)
@@ -191,31 +202,17 @@ class T(configparser.ConfigParser):
         self.changed("main_window")
 
     def get_list(self, name):
-        i = 0
-        list = []
-        while(True):
-            try:
-                key = "%d" % i
-                val = self.get(name, key)
-                list.append(val)
-                i += 1
-            except configparser.NoOptionError:
-                return list
+        return list(self[name])
 
     def remove_all_in_list_section(self, name):
-        i = 0
-        items_left = True
-        while items_left:
-            items_left = self.remove_option(name, "%d" % i)
-            i += 1
+        for item in self[name]:
+            self.remove_option(name, item)
 
     def set_list(self, name, list):
         self.remove_all_in_list_section(name)
 
-        i = 0
         for item in list:
-            configparser.ConfigParser.set(self, name, "%d" % i, item)
-            i += 1
+            configparser.ConfigParser.set(self, name, item, None)
 
         self.changed(name)
 

fract4d/tests/test_fractconfig.py
@@ -73,3 +73,11 @@ class Test(testbase.TestSetup):
     def testDarwin(self):
         c = fractconfig.DarwinConfig("testprefs")
         self.assertEqual("open -e", c.get_default_editor())
+
+    def testUpdatePaths(self):
+        self.userConfig["formula_path"] = {
+            "0": "formulas", "1": "/usr/share/gnofract4d", "2":"/home/fract4d/formulas"}
+        self.userConfig.update_paths("formula_path")
+        self.assertEqual(
+            self.userConfig["formula_path"],
+            {"formulas": None, "/usr/share/gnofract4d": None, "/home/fract4d/formulas": None})

fract4d/tests/testbase.py
@@ -255,8 +255,8 @@ class ClassSetup(TestBase):
         cls.userConfig.set("general", "cache_dir",
                            os.path.join(cls.tmpdir.name, "gnofract4d-cache"))
 
-        cls.userConfig["formula_path"] = {"0": "formulas",
-                                          "1": "testdata/formulas"}
+        cls.userConfig["formula_path"] = {"formulas": None,
+                                          "testdata/formulas": None}
         cls.g_comp = fc.Compiler(cls.userConfig)
 
     @classmethod
@@ -277,7 +277,7 @@ class TestSetup(TestBase):
 
         self.userConfig.set("general", "cache_dir",
                             os.path.join(self.tmpdir.name, "gnofract4d-cache"))
-        self.userConfig["formula_path"] = {"0": "formulas"}
+        self.userConfig["formula_path"] = {"formulas": None}
         self.g_comp = fc.Compiler(self.userConfig)
 
     def tearDown(self):

commit 0ad5a8534edd6f85a88961d42d2453640f42665e

Make configuration file keys case sensitive
Fixes problems with file paths caused by:
3e3509e ("Fix duplicating formula_paths in user configuration (#217)", 2021-02-04)

fract4d/fractconfig.py
@@ -165,6 +165,10 @@ class T(configparser.ConfigParser):
         # appears to work for most unixes
         return "-fPIC -DPIC -O2 -shared -ffast-math"
 
+    def optionxform(self, option):
+        # make keys case-sensitive because some are file paths
+        return str(option)
+
     def set(self, section, key, val):
         if self.has_section(section) and \
            self.has_option(section, key) and \